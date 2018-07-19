45eb9d9a1ed8a11b2a5c95a9261dc1f21d558253
161250121 <161250121@smail.nju.edu.cn>
2018-07-20 07:35:39 +0800

    添加数据层工厂

---
+++ src/main/java/data/BillData/FileHelper/NormalFileHelper.java
@@ -0,0 +1,12 @@
+package data.BillData.FileHelper;
+
+import java.io.File;
+
+public class NormalFileHelper {
+    public static File crreateFileByPath(String path){
+        return null;
+    }
+    public String getProjectPath(){
+        return this.getClass().getResource("/").getPath();
+    }
+}
