b870a2edc8c182431f9a33d446fddf2c34cd2b07
Enigma <zhangzhao0901@qq.com>
2018-07-26 17:24:49 +0800

    修改了注册bug新添加登录窗体

---
+++ src/main/java/ui/controller/UserLogin.java
@@ -0,0 +1,35 @@
+package ui.controller;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import ui.Main;
+import vo.UserVO;
+
+import logicServiceFactory.UserLogicServiceFactory;
+
+public class UserLogin {
+	@FXML
+	private TextField u_name;
+	@FXML
+	private TextField u_pass;
+	
+	public void login() {
+		u_name=new TextField();
+		u_pass=new TextField();
+		if(UserLogicServiceFactory.getUserLogicService().logIn(new UserVO(u_name.getText(),u_pass.getText()))==true) {
+			System.out.println("ok");
+		}else {
+			System.out.println("no");
+		}
+		
+	}
+	public void return_main() throws Exception{
+		Parent returnmain= FXMLLoader.load(getClass().getResource("../../newUser.fxml"));
+        Main.getPrimaryStage().setScene(new Scene(returnmain));
+	}
+}
